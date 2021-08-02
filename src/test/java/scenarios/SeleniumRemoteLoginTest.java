777b1190fef434ea2f6214c4a42de8188728bbe1
furkanbırgul <brgl.furkan@gmail.com>
2021-08-02 14:09:43 -0400

    fixed

--- src/test/java/scenarios/SeleniumRemoteLoginTest.java
+++ src/test/java/scenarios/SeleniumRemoteLoginTest.java
@@ -1,10 +1,11 @@
 package scenarios;
 
 import core.BaseTest;
+import core.SeleniumRemoteBaseTest;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
-public class SeleniumRemoteLoginTest extends BaseTest{
+public class SeleniumRemoteLoginTest extends SeleniumRemoteBaseTest {
 
     final Logger LOGGER = Logger.getLogger(SeleniumRemoteLoginTest.class);
 
